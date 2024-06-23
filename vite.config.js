a48508300f7f354b3d262442fca3fae92b38492d
Marek Rzepka <marekip3@gmail.com>
2024-06-24 00:49:52 +0200

    2

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,13 @@
-import { join } from "node:path";
-import { buildSync } from "esbuild";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    {
-      apply: "build",
-      enforce: "post",
-      transformIndexHtml() {
-        buildSync({
-          minify: true,
-          bundle: true,
-          entryPoints: [join(process.cwd(), "sw.js")],
-          outfile: join(process.cwd(), "dist", "sw.js"),
-        });
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        manifest: "public/manifest.json",
       },
     },
-  ],
+  },
 });
