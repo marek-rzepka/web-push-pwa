3b08b0646b2c8bfbacbc32242eb59fa8f13981e2
marek-rzepka <m.rzepka@gamefound.com>
2024-11-07 21:50:53 +0100

    wip

--- main.js
+++
@@ -1,8 +0,0 @@
-import "./services/firebase";
-import { getPermission } from "./services/cloudMessaging";
-import { register } from "register-service-worker";
-import './services/webPushIOS';
-
-register(`./sw.js`);
-
-document.getElementById("notify").addEventListener("click", getPermission);
